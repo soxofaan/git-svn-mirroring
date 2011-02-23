6f515339f9901455b680b42b2a54a4a5bc5d36a1
Stefaan Lippens <soxofaan@gmail.com>
2011-02-23 01:32:06 +0100

    made clone-svn-to-git.sh in line with master-sync.sh

--- clone-svn-to-git.sh
+++ clone-svn-to-git.sh
@@ -4,25 +4,13 @@
 set -x
 
 # Clean up output from previous runs before we start.
-rm -fr git-repo git-wc
+rm -fr git-repo
 
-# Get absulute path of current working directory.
 root=$(pwd)
 
-
-##############################################################
-# Git-svn proxy: git-repo
 cd $root
 git svn clone --prefix=svn/ file://$root/svn-repo git-repo
 
-# Initialize "pointer" to last synced commit
-# and check that branch out (so master is pushable)
 cd $root/git-repo
-git branch svn/last-sync master
-git checkout svn/last-sync
-
-##############################################################
-# Git working copy
-cd $root
-git clone git-repo git-wc
-
+git co -b svn-sync-branch master
+git branch svn-last-sync master
