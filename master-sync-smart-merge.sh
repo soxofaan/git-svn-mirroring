00165919d8b0af79af96d8094b2b532fb94741bc
Stefaan Lippens <soxofaan@gmail.com>
2011-02-23 14:12:23 +0100

    used variables instead of hard coded branch names

--- master-sync-smart-merge.sh
+++ master-sync-smart-merge.sh
@@ -5,15 +5,25 @@ die() {
 	exit 1
 }
 
+# The git master branch that has to be synced to SVN.
+gitmaster=master
+# The branch that is used to interface with SVN through git-svn rebase and dcommit.
+svnsyncbranch=svn-sync-branch
+# Pointer to the last synced commit of the git master branch.
+svnlastsync=svn-last-sync
+# Temporary work branch that will be ported with rebase from $gitmaster to $svnsyncbranch.
+workbranch=svn-tmp
+
+# Handy for dedbugging
 set -x
 
 
 # Set a temporary working branch, pointing at current master.
 # Note that this branch also acts as sort of mutex.
-git branch svn-tmp master || die 'Could not create temporary working branch, maybe another sync is in progress?'
+git branch $workbranch $gitmaster || die 'Could not create temporary working branch, maybe another sync is in progress?'
 
 # Rebase the commits between last sync point and current master on top of the svn sync branch.
-git rebase --onto svn-sync-branch svn-last-sync svn-tmp
+git rebase --onto $svnsyncbranch $svnlastsync $workbranch
 successfulrebase=$?
 
 if [ $successfulrebase -ne 0 ]; then
@@ -21,29 +31,29 @@ if [ $successfulrebase -ne 0 ]; then
 	git rebase --abort
 
 	# Start over: reset working branch to last sync point
-	git checkout svn-last-sync
-	git branch -f svn-tmp svn-last-sync
-	git checkout svn-tmp
+	git checkout $svnlastsync
+	git branch -f $workbranch $svnlastsync
+	git checkout $workbranch
 	# Now squash the new commits to one commit
 	# (to avoid the rebase problems) and commit on the temporary branch 
-	git merge --squash master
+	git merge --squash $gitmaster
 	git commit -F .git/SQUASH_MSG
 
 	# Rebase the squashed commit on top of the svn sync branch.
-	git rebase --onto svn-sync-branch svn-last-sync svn-tmp
+	git rebase --onto $svnsyncbranch $svnlastsync $workbranch
 fi
 
 # Fast forward the svn sync branch with the squashed commit.
-git checkout svn-sync-branch
-git merge --ff-only svn-tmp
+git checkout $svnsyncbranch
+git merge --ff-only $workbranch
 
 # Sync the suqashed commit to Subversion
 git svn dcommit
 
 # Update the pointer to the last synced commit
-git branch -f svn-last-sync master
-git checkout svn-last-sync
+git branch -f $svnlastsync $gitmaster
+git checkout $svnlastsync
 
 # Clean up temporary work branch (release mutex).
-git branch -D svn-tmp
+git branch -D $workbranch
 
