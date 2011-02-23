70ae29ea9bf82b90d9995c024d68bcd4f6e9a3e5
Stefaan Lippens <soxofaan@gmail.com>
2011-02-23 12:03:14 +0100

    simplyfied default rebase approach

--- master-sync-smart-merge.sh
+++ master-sync-smart-merge.sh
@@ -10,35 +10,29 @@ die() {
 set -x
 
 
-# Create a temporary branch, starting from last sync point on master
-git branch -f svn-tmp svn-last-sync
-git checkout svn-tmp
+# Set a temporary working branch, pointing at current master
+git branch -f svn-tmp master
 
-# First try with normal merge
-git merge master
-
-# Rebase the squashed commit on top of the svn sync branch.
+# Rebase the commits between last sync point and current master on top of the svn sync branch.
 git rebase --onto svn-sync-branch svn-last-sync svn-tmp
 successfulrebase=$?
 
 if [ $successfulrebase -ne 0 ]; then
+	# Undo rebase
 	git rebase --abort
 
-	# Start over and do a squashed merge.
+	# Start over: reset working branch to last sync point
 	git checkout svn-last-sync
 	git branch -f svn-tmp svn-last-sync
 	git checkout svn-tmp
-
-	# Squash the new commits on master to one commit 
+	# Now squash the new commits to one commit
 	# (to avoid the rebase problems) and commit on the temporary branch 
 	git merge --squash master
 	git commit -F .git/SQUASH_MSG
 
 	# Rebase the squashed commit on top of the svn sync branch.
 	git rebase --onto svn-sync-branch svn-last-sync svn-tmp
-
 fi
-
 
 
 # Fast forward the svn sync branch with the squashed commit.
